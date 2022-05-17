c139b134ad65051a746e27515681966526165e93
JoasSchmidt <101359176+JoasSchmidt@users.noreply.github.com>
2022-05-17 16:52:30 -0300

    Add files via upload

---
+++ player.js
@@ -0,0 +1,17 @@
+class Player{
+    constructor(x, y, width, height) {
+        const options = {
+          isStatic: true
+        };
+    
+        this.body = Matter.Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        this.collapse = false;
+        this.image = loadImage("./assets/playerArcher.png");
+    
+        World.add(world, this.body);
+    
+        Matter.Body.setAngle(this.body, -90);
+      }
+}
